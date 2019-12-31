6773e632ab71af55a12103e1977cdcb9a7c5d31f
thallysbraz <thallysbraz3@gmail.com>
2019-12-31 09:46:26 -0300

    Criado cadastro de categorias e Slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const slugify = require("slugify");
 const router = express.Router();
+
+//Importando Model de Categoria
+const Category = require("./Category");
 
 router.get("/", (req, res) => {
   res.send("Rota de categoria");
@@ -8,6 +11,27 @@ router.get("/", (req, res) => {
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.post("/save", (req, res) => {
+  const title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugify(title)
+    })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(error => {
+        res.status(404).json({
+          msg: "Error ao pesquisar resposta, por favor, refazer a busca",
+          error: error
+        });
+      });
+  } else {
+    res.redirect("/categories/admin/categories/new");
+  }
 });
 
 module.exports = router;
