096c920b00234b97cd480f2e164aefebb820ddcf
thallysbraz <thallysbraz3@gmail.com>
2020-01-02 09:52:38 -0300

    criando form de artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,5 +5,14 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Rota de Artigo");
 });
+//rota para renderizar view de add artigo
+
+router.get("/admin/articles/new", (req, res) => {
+  res.render("admin/articles/new");
+});
+
+router.post("/save", (req, res) => {
+  console.log("ok, save");
+});
 
 module.exports = router;
