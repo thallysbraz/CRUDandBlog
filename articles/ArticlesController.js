792e9e058bd3d905208468777262547b3c4b298d
thallysbraz <thallysbraz3@gmail.com>
2020-01-02 10:19:32 -0300

    finalizando form de artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,13 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
+//Carregando Models
+const Category = require("../categories/Category"); // Categoria
+const Article = require("./Article"); // Artigo
+
+//rotas
 router.get("/", (req, res) => {
   res.send("Rota de Artigo");
 });
-//rota para renderizar view de add artigo
 
+//rota para renderizar view de add artigo
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  //Pesquisando as categorias e passando para a view
+  Category.findAll()
+    .then(categories => {
+      res.render("admin/articles/new", {
+        categories: categories
+      });
+    })
+    .catch(error => {
+      // se der erro interno, mostra json ao usuário com o erro.
+      res.status(404).json({
+        msg: "Error interno ao listar as categorias",
+        error: error
+      });
+    });
 });
 
 router.post("/save", (req, res) => {
