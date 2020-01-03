aaeda6b622ed2237acb6c8618752adb64852540d
thallysbraz <thallysbraz3@gmail.com>
2020-01-03 11:33:31 -0300

    hash e iniciando verificações

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 //Model de User
 const User = require("./User");
@@ -15,12 +16,38 @@ router.get("/users/create", (req, res) => {
   res.render("admin/users/create");
 });
 router.post("/users/create", (req, res) => {
-  var { email, password } = req.body;
+  try {
+    var { email, password } = req.body; // recebendo dados do form
+    var salt = bcrypt.genSaltSync(10); // Salt para "aumentar" a segurança da criptografia
+    var hash = bcrypt.hashSync(password, salt); // cryptografando a senha
 
-  console.log("email: " + email);
-  console.log("password: " + password);
+    //verificando se email está definido
+    if (email == undefined) {
+      res.redirect("/admin/users/create");
+    }
+    //verificando se senha está definida
+    else if (password == undefined) {
+      res.redirect("/admin/users/create");
+    }
 
-  res.redirect("/admin/users/create");
+    //Salvando user no Banco
+    User.create({
+      email: email,
+      password: hash
+    })
+      .then(() => {
+        res.redirect("/admin/users/create");
+      })
+      .catch(error => {
+        // se der erro interno, mostra json ao usuário com o erro.
+        res.status(404).json({
+          msg: "Error interno ao salvar usuário",
+          error: error
+        });
+      });
+  } catch (error) {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
