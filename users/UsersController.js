ce851972e1215545bd39d37e01896a9d95e7015e
thallysbraz <thallysbraz3@gmail.com>
2020-01-03 11:46:31 -0300

    fazendo verificação de email

--- users/UsersController.js
+++ users/UsersController.js
@@ -8,13 +8,26 @@ const router = express.Router();
 
 //rota pra listar os usuários
 router.get("/users", (req, res) => {
-  res.send("User list");
+  User.findAll({
+    order: [["id", "DESC"]]
+  })
+    .then(users => {
+      res.render("admin/users/index", { users: users });
+    })
+    .catch(error => {
+      // se der erro interno, mostra json ao usuário com o erro.
+      res.status(404).json({
+        msg: "Error interno ao buscar os usuário",
+        error: error
+      });
+    });
 });
 
 //rota para view de adicionar usuários
 router.get("/users/create", (req, res) => {
   res.render("admin/users/create");
 });
+//rota com method=post para salvar user no BD
 router.post("/users/create", (req, res) => {
   try {
     var { email, password } = req.body; // recebendo dados do form
@@ -30,18 +43,37 @@ router.post("/users/create", (req, res) => {
       res.redirect("/admin/users/create");
     }
 
-    //Salvando user no Banco
-    User.create({
-      email: email,
-      password: hash
-    })
-      .then(() => {
-        res.redirect("/admin/users/create");
+    //verificando se user ja é cadastrado
+
+    User.findOne({ where: { email: email } })
+      .then(user => {
+        // se encontrar algum email, redireciona o usuário para o form
+        if (user) {
+          res.redirect("/admin/users/create");
+        }
+        // se não encontrar salva user no Banco de Dados
+        else {
+          //Salvando user no Banco
+          User.create({
+            email: email,
+            password: hash
+          })
+            .then(() => {
+              res.redirect("/");
+            })
+            .catch(error => {
+              // se der erro interno, mostra json ao usuário com o erro.
+              res.status(404).json({
+                msg: "Error interno ao salvar usuário",
+                error: error
+              });
+            });
+        }
       })
       .catch(error => {
         // se der erro interno, mostra json ao usuário com o erro.
         res.status(404).json({
-          msg: "Error interno ao salvar usuário",
+          msg: "Error interno ao validar o usuário",
           error: error
         });
       });
